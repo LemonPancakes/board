c7f2efab39d2306305e05eb8f8249d1f0a629d9c
Adam He <shuaihe2018@u.northwestern.edu>
2019-05-01 15:56:17 -0700

    initial commit

---
+++ connect6/connect6.go
@@ -0,0 +1,140 @@
+package connect6
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Move struct {
+	x, y int
+}
+
+func (m *Move) Add(a *Move) {
+	m.x += a.x
+	m.y += a.y
+}
+
+func (m *Move) Sub(a *Move) {
+	m.x -= a.x
+	m.y -= a.y
+}
+
+type Connect6 struct {
+	board         [19][19]int
+	CurrentPlayer int
+	firstMove     bool
+	Finished      bool
+}
+
+func (game *Connect6) NewGame() {
+	var tmp [19][19]int
+	game.board = tmp
+	game.CurrentPlayer = 1
+	game.firstMove = false
+	game.Finished = false
+}
+
+func (game *Connect6) MakeMove(m Move) (int, error) {
+	if game.Finished {
+		return -1, errors.New("Game is already finished")
+	}
+
+	if game.board[m.x][m.y] != 0 {
+		return -1, errors.New("Space is already taken")
+	}
+
+	player := game.CurrentPlayer
+	game.board[m.x][m.y] = player
+	if game.CheckWin(m) {
+		game.Finished = true
+		return player, nil
+	}
+
+	if !game.firstMove {
+		game.CurrentPlayer = 3 - game.CurrentPlayer
+	}
+	game.firstMove = !game.firstMove
+	return player, nil
+}
+
+func (game *Connect6) Print() {
+	for i := 0; i < 19; i++ {
+		for j := 0; j < 19; j++ {
+			switch curr := game.board[i][j]; curr {
+			case 0:
+				fmt.Print(".")
+			case 1:
+				fmt.Print("X")
+			case 2:
+				fmt.Print("O")
+			default:
+				fmt.Println(errors.New("WTF???"))
+			}
+		}
+		fmt.Println("")
+	}
+}
+
+func (game *Connect6) CheckWin(m Move) bool {
+	var directions = [4]Move{{0, 1}, {1, 0}, {1, 1}, {1, -1}}
+	for _, dir := range directions {
+		currLength := 1
+		tmp := m
+		tmp.Add(&dir)
+		// Check in forward direction first
+		for currLength < 6 && tmp.x < 19 && tmp.x >= 0 && tmp.y < 19 && tmp.y >= 0 && game.board[tmp.x][tmp.y] == game.CurrentPlayer {
+			tmp.Add(&dir)
+			currLength++
+		}
+
+		tmp = m
+		tmp.Sub(&dir)
+		// Check in opposite direction
+		for currLength < 6 && tmp.x < 19 && tmp.x >= 0 && tmp.y < 19 && tmp.y >= 0 && game.board[tmp.x][tmp.y] == game.CurrentPlayer {
+			tmp.Sub(&dir)
+			currLength++
+		}
+
+		if currLength == 6 {
+			return true
+		}
+	}
+	return false
+}
+
+func (game *Connect6) GetState() string {
+	gameState := ""
+
+	gameState += strconv.Itoa(game.CurrentPlayer) + ","
+
+	if game.firstMove {
+		gameState += "1"
+	} else {
+		gameState += "0"
+	}
+	gameState += ","
+
+	if game.Finished {
+		gameState += "1"
+	} else {
+		gameState += "0"
+	}
+
+	for i := 0; i < 19; i++ {
+		for j := 0; j < 19; j++ {
+			gameState += "," + strconv.Itoa(game.board[i][j])
+		}
+	}
+
+	return gameState
+}
+
+func ParseMove(s string) Move {
+	inputs := strings.Split(strings.TrimSpace(s), ",")
+	row, _ := strconv.Atoi(inputs[0])
+	col, _ := strconv.Atoi(inputs[1])
+	fmt.Printf("Move at (%d, %d)\n", row, col)
+	return Move{row, col}
+}
---
+++ main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/adamhe17/connect6"
+	"github.com/google/uuid"
+	"github.com/gorilla/sessions"
+	"github.com/gorilla/websocket"
+)
+
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+	players    [2]string
+}
+
+func (manager *ClientManager) start() {
+	for {
+		select {
+		case conn := <-manager.register:
+			manager.clients[conn] = true
+			jsonMessage, _ := json.Marshal(&Message{
+				Type:    mtGameState,
+				Content: strconv.Itoa(conn.player) + "," + game.GetState(),
+			})
+			manager.send(jsonMessage, conn)
+		case conn := <-manager.unregister:
+			if _, ok := manager.clients[conn]; ok {
+				close(conn.send)
+				delete(manager.clients, conn)
+			}
+		case message := <-manager.broadcast:
+			for conn := range manager.clients {
+				select {
+				case conn.send <- message:
+				}
+			}
+		}
+	}
+}
+
+func (manager *ClientManager) send(message []byte, conn *Client) {
+	conn.send <- message
+}
+
+func (manager *ClientManager) sendAll(message []byte) {
+	for conn := range manager.clients {
+		conn.send <- message
+	}
+}
+
+func (manager *ClientManager) sendToPlayer(message []byte, player int) {
+	for conn := range manager.clients {
+		if conn.id == players[player-1] {
+			conn.send <- message
+		}
+	}
+}
+
+func (manager *ClientManager) addPlayer(client *Client) {
+	uid := client.id
+	if players[0] == "" || players[0] == uid {
+		players[0] = uid
+		client.player = 1
+	} else if players[1] == "" || players[1] == uid {
+		players[1] = uid
+		client.player = 2
+	} else {
+		client.player = -1
+	}
+}
+
+type Client struct {
+	id     string
+	socket *websocket.Conn
+	send   chan []byte
+	player int
+}
+
+func (c *Client) read() {
+	defer func() {
+		manager.unregister <- c
+		c.socket.Close()
+	}()
+
+	for {
+		_, message, err := c.socket.ReadMessage()
+		if err != nil {
+			manager.unregister <- c
+			c.socket.Close()
+			break
+		}
+
+		moveString := string(message)
+		move := connect6.ParseMove(moveString)
+		player, gameErr := game.MakeMove(move)
+		var messageJSON []byte
+		if gameErr != nil {
+			messageJSON, _ = json.Marshal(&Message{
+				Sender:  c.id,
+				Type:    mtError,
+				Content: gameErr.Error(),
+			})
+		} else {
+			messageJSON, _ = json.Marshal(&Message{
+				Sender:  c.id,
+				Type:    mtMove,
+				Content: moveString + "," + strconv.Itoa(player),
+			})
+			manager.broadcast <- messageJSON
+
+			if game.Finished {
+				messageJSON, _ = json.Marshal(&Message{
+					Sender:  c.id,
+					Type:    mtFinished,
+					Content: strconv.Itoa(game.CurrentPlayer),
+				})
+			} else {
+				messageJSON, _ = json.Marshal(&Message{
+					Sender:  c.id,
+					Type:    mtCurrentPlayer,
+					Content: strconv.Itoa(game.CurrentPlayer),
+				})
+			}
+		}
+		manager.broadcast <- messageJSON
+	}
+}
+
+func (c *Client) write() {
+	defer func() {
+		c.socket.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.socket.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			c.socket.WriteMessage(websocket.TextMessage, message)
+		}
+	}
+}
+
+type messageType string
+
+type Message struct {
+	Sender    string      `json:"sender,omitempty"`
+	Recipient string      `json:"recipient,omitempty"`
+	Type      messageType `json:"type,omitempty"`
+	Content   string      `json:"content,omitempty"`
+}
+
+const (
+	mtError         messageType = "Error"
+	mtGameState     messageType = "GameState"
+	mtMove          messageType = "Move"
+	mtFinished      messageType = "Finished"
+	mtCurrentPlayer messageType = "CurrentPlayer"
+)
+
+var (
+	manager = ClientManager{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+
+	game    = connect6.Connect6{}
+	players [2]string
+
+	sessionKey   = []byte("lemonpancakes")
+	sessionStore = sessions.NewCookieStore(sessionKey)
+)
+
+func main() {
+	fmt.Println("Starting game")
+	game.NewGame()
+
+	fmt.Println("Starting application...")
+	go manager.start()
+	http.HandleFunc("/ws", wsPage)
+	http.ListenAndServe(":12345", nil)
+}
+
+func wsPage(res http.ResponseWriter, req *http.Request) {
+	conn, error := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}).Upgrade(res, req, nil)
+	if error != nil {
+		http.NotFound(res, req)
+		return
+	}
+
+	fmt.Println(req.Body)
+
+	uid, _ := uuid.NewRandom()
+	client := &Client{
+		id:     uid.String(),
+		socket: conn,
+		send:   make(chan []byte),
+	}
+	manager.addPlayer(client)
+
+	manager.register <- client
+
+	go client.read()
+	go client.write()
+}
